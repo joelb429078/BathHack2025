8088241328dfb58305968761f59bc608d173600d
joelb429078 <joelbiju04@gmail.com>
2025-04-06 11:14:13 +0100

    Ui revamp fix up - previous push was corrupted

--- quayo/src/pages/home.js
+++ quayo/src/pages/home.js
@@ -760,6 +760,18 @@ const Home = () => {
           -webkit-box-orient: vertical;
           overflow: hidden;
         }
+        .line-clamp-3 {
+          display: -webkit-box;
+          -webkit-line-clamp: 1;
+          -webkit-box-orient: vertical;
+          overflow: hidden;
+        }
+        .line-clamp-4 {
+          display: -webkit-box;
+          -webkit-line-clamp: 2;
+          -webkit-box-orient: vertical;
+          overflow: hidden;
+        }
       `}</style>
     </div>
   );
